43637c34fff9a5bc8254c30bfe5fea431796e8be
Alanolog <alanrutyna2003@gmail.com>
2021-09-15 18:29:11 +0200

    making imgs work

--- src/components/molecules/FourStepsContent.js
+++ src/components/molecules/FourStepsContent.js
@@ -1,25 +1,29 @@
 import React from "react";
 import OneStepContent from "../atoms/OneStepContent";
+import icon1 from "../../assets/Icon-1.svg";
+import icon2 from "../../assets/Icon-2.svg";
+import icon3 from "../../assets/Icon-3.svg";
+import icon4 from "../../assets/Icon-4.svg";
 function FourStepsContent() {
   return (
     <div className="four-steps-content">
       <OneStepContent
-        imageUrl="../src/assets/Icon-1.svg"
+        imageUrl={icon1}
         heading="Wybierz rzeczy"
         text="ubrania, zabawki, sprzęt i inne"
       />
       <OneStepContent
-        imageUrl="../src/assets/Icon-2.svg"
+        imageUrl={icon2}
         heading="Spakuj je"
         text="skorzystaj z worków na śmieci"
       />
       <OneStepContent
-        imageUrl="../src/assets/Icon-3.svg"
+        imageUrl={icon3}
         heading="Zdecyduj komu chcesz pomóc"
         text="wybierz zaufane miejsce"
       />
       <OneStepContent
-        imageUrl="../src/assets/Icon-4.svg"
+        imageUrl={icon4}
         heading="Zamów kuriera"
         text="kurier przyjedzie w dogodnym terminie"
       />
