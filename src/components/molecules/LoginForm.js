a6f455b633690b6f1a9fa5e57406b24dc554ac8b
Alanolog <alanrutyna2003@gmail.com>
2021-09-18 12:55:18 +0200

    adding form working

--- src/components/molecules/LoginForm.js
+++ src/components/molecules/LoginForm.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <form>
       <div className="login-content-form-box">
         <label htmlFor="email">
           Email
-          <input type="text" name="email" />
+          <input
+            type="text"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <label htmlFor="password">
           Hasło
-          <input type="password" name="password" />
+          <input
+            type="password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
       </div>
     </form>
