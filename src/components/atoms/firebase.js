270c2d81e3a43c0ca859938b194c6bc6cd5c622b
Alanolog <alanrutyna2003@gmail.com>
2021-09-20 16:25:03 +0200

    adding firebase to project

---
+++ src/components/atoms/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+const config = {
+  apiKey: "AIzaSyDSAwhNNnJB8mx04z5xw51H7b6ZRPk41Uk",
+  authDomain: "giving-site-a593d.firebaseapp.com",
+  projectId: "giving-site-a593d",
+  storageBucket: "giving-site-a593d.appspot.com",
+  messagingSenderId: "491381958578",
+  appId: "1:491381958578:web:d4c1461b735d5dbef926ab",
+  measurementId: "G-50ND9BCTF7",
+};
+firebase.initializeApp(config);
+
+export default firebase;
