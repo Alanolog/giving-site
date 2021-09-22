e4e62723c407880fb3e42dc6c1cc17f31200da71
Alanolog <alanrutyna2003@gmail.com>
2021-09-22 11:19:48 +0200

    adding give items form warning

---
+++ src/components/molecules/GiveItemsWarning.js
@@ -0,0 +1,13 @@
+import React from "react";
+function GiveItemsWarning() {
+  return (
+    <section className="giveItems-warning">
+      <h1>Ważne!</h1>
+      <h3>
+        Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy
+        wiedzieć komu najlepiej je przekazać.
+      </h3>
+    </section>
+  );
+}
+export default GiveItemsWarning;
--- src/components/templates/GiveItems.js
+++ src/components/templates/GiveItems.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroGiveItems from "../organisms/HeroGiveItems";
+import GiveItemsWarning from "../molecules/GiveItemsWarning";
 import FormBox from "../organisms/FormBox";
 function GiveItems({ user }) {
   return (
     <>
       <HeroGiveItems user={user} />
+      <GiveItemsWarning />
       <FormBox />
     </>
   );
