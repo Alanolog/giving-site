70546ce124f00021ca4255b6ff36e6e0fec9176b
Alanolog <alanrutyna2003@gmail.com>
2021-09-25 21:54:24 +0200

    fixing four steps router

--- src/components/organisms/FourSteps.js
+++ src/components/organisms/FourSteps.js
@@ -2,24 +2,21 @@ import React from "react";
 import FourStepsHeader from "../molecules/FourStepsHeader";
 import FourStepsContent from "../molecules/FourStepsContent";
 import Button from "../atoms/Button";
-import { BrowserRouter as Router } from "react-router-dom";
 function FourSteps() {
   return (
     <section className="four-steps" name="four-steps">
       <FourStepsHeader />
       <FourStepsContent />
-      <Router>
-        <div
-          style={{
-            width: "100%",
-            display: "flex",
-            justifyContent: "center",
-            marginBottom: "80px",
-          }}
-        >
-          <Button text="Oddaj Rzeczy" link="/logowanie" />
-        </div>
-      </Router>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          marginBottom: "80px",
+        }}
+      >
+        <Button text="Oddaj Rzeczy" link="/logowanie" />
+      </div>
     </section>
   );
 }
