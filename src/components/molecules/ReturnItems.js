2c029084556b9454513a45647b0dc416f9d76d1f
Alanolog <alanrutyna2003@gmail.com>
2021-09-25 21:53:31 +0200

    removing unused empty component

--- src/components/molecules/ReturnItems.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-function ReturnItems() {
-  return <></>;
-}
-
-export default ReturnItems;
--- src/components/templates/Home.js
+++ src/components/templates/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Hero from "../organisms/Hero";
 import InfoCards from "../organisms/InfoCards";
-import ReturnItems from "../molecules/ReturnItems";
 import AboutUs from "../organisms/AboutUs";
 import HelpInfo from "../organisms/HelpInfo";
 import FormBox from "../organisms/FormBox";
@@ -10,7 +9,6 @@ function Home({ user, setUser }) {
     <>
       <Hero user={user} setUser={setUser} />
       <InfoCards />
-      <ReturnItems />
       <AboutUs />
       <HelpInfo />
       <FormBox />
